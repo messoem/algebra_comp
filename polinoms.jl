5b329611876ae27657430b2a80a21eccf40f6398
reaper666 <57996364+novichok228@users.noreply.github.com>
2022-12-15 10:20:34 +0300

    Update polinoms.jl

--- polinoms.jl
+++ polinoms.jl
@@ -3,7 +3,38 @@ Base.@kwdef mutable struct Polinomial_of_one_variable
 end
 
 function print_degree(p::Polinomial_of_one_variable)
-    return length(p.coef)
+    println(length(p.coef))
+end
+
+
+function print_poly(p::Polinomial_of_one_variable)
+    poly = ""
+    if length(p.coef) != 1
+        for i in 1:(length(p.coef))
+            if p.coef[i] != 0
+                if i == 1
+                    poly *= string(p.coef[i])
+                elseif i == 2
+                    poly *= " + "*string(p.coef[i])*"x"
+                elseif  i != length(p.coef)
+                    poly*= " + "*string(p.coef[i])*"x^"*string(i-1)
+                else
+                    poly*= " + "*string(p.coef[i])*"x^"*string(i-1)
+                end
+            else
+                if i == 1
+                    poly *= string(0)*" + "
+                elseif i == 2
+                    poly *= ""
+                elseif  i != length(p.coef)
+                    poly*= ""
+                else
+                    poly*= ""
+                end
+            end
+        end
+    end
+    return poly
 end
 
 function sum_of_pol(p1::Polinomial_of_one_variable, p2::Polinomial_of_one_variable)::Polinomial_of_one_variable
@@ -64,10 +95,10 @@ function multiplication_of_pol(p1::Polinomial_of_one_variable, p2::Polinomial_of
 end
 
 
+
 p = Polinomial_of_one_variable([1, 3, 2])
 p1 = Polinomial_of_one_variable([4, 5, 2])
 println("------------")
-
 println(p.coef)
 println("------------")
 print_degree(p)
@@ -77,3 +108,7 @@ println("------------")
 println(subtraction_of_pol(p1, p).coef)
 println("------------")
 println(multiplication_of_pol(p, p1).coef)
+println("------------")
+print_poly(p)
+print_poly(subtraction_of_pol(p1, p))
+print_poly(multiplication_of_pol(p, p1))
