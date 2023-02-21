1809b139abcf77564fa32f38554ee39d39c9bc2e
reaper666 <57996364+novichok228@users.noreply.github.com>
2023-02-21 22:03:22 +0300

    Update polinoms.jl

--- polinoms.jl
+++ polinoms.jl
@@ -1,27 +1,27 @@
-Base.@kwdef mutable struct Polinomial_of_one_variable
+Base.@kwdef mutable struct Polynom
     coef::Vector{BigInt}
 end
 
-function print_degree(p::Polinomial_of_one_variable)
-    println(length(p.coef))
-end
 
 
-function print_poly(p::Polinomial_of_one_variable)
+
+ord(p::Polynom) = length(p.coef) - 1
+
+function Base.display(p::Polynom)
     poly = ""
     if length(p.coef) != 1
         for i in 1:(length(p.coef))
-            if p.coef[i] != 0
+            if p.coef[i] > 0
                 if i == 1
                     poly *= string(p.coef[i])
                 elseif i == 2
-                    poly *= " + "*string(p.coef[i])*"x"
+                    poly *= " + " * string(p.coef[i])*"x"
                 elseif  i != length(p.coef)
-                    poly*= " + "*string(p.coef[i])*"x^"*string(i-1)
+                    poly*= " + " * string(p.coef[i])*"x^"*string(i-1)
                 else
-                    poly*= " + "*string(p.coef[i])*"x^"*string(i-1)
+                    poly*= " + " * string(p.coef[i])*"x^"*string(i-1)
                 end
-            else
+            elseif p.coef[i] == 0
                 if i == 1
                     poly *= string(0)*" + "
                 elseif i == 2
@@ -31,13 +31,23 @@ function print_poly(p::Polinomial_of_one_variable)
                 else
                     poly*= ""
                 end
+            else
+                if i == 1
+                    poly *= string(p.coef[i])
+                elseif i == 2
+                    poly *= " - "*string(abs(p.coef[i]))*"x"
+                elseif  i != length(p.coef)
+                    poly*= " - "*string(abs(p.coef[i]))*"x^"*string(i-1)
+                else
+                    poly*= " - "*string(abs(p.coef[i]))*"x^"*string(i-1)
+                end
             end
         end
     end
     println(poly)
 end
 
-function sum_of_pol(p1::Polinomial_of_one_variable, p2::Polinomial_of_one_variable)::Polinomial_of_one_variable
+function Base. +(p1::Polynom, p2::Polynom)
     lst = []
     if length(p1.coef) >= length(p2.coef)
         for i in 1:length(p1.coef)
@@ -57,11 +67,11 @@ function sum_of_pol(p1::Polinomial_of_one_variable, p2::Polinomial_of_one_variab
             end
         end
     end
-    return Polinomial_of_one_variable(lst)
+    return Polynom(lst)
 end
 
 
-function subtraction_of_pol(p1::Polinomial_of_one_variable, p2::Polinomial_of_one_variable)::Polinomial_of_one_variable
+function Base. -(p1::Polynom, p2::Polynom)::Polynom
     lst= []
     if length(p1.coef) >= length(p2.coef)
         for i in 1:length(p1.coef)
@@ -81,34 +91,17 @@ function subtraction_of_pol(p1::Polinomial_of_one_variable, p2::Polinomial_of_on
             end
         end
     end
-    return Polinomial_of_one_variable(lst)
+    return Polynom(lst)
 end
 
-function multiplication_of_pol(p1::Polinomial_of_one_variable, p2::Polinomial_of_one_variable)::Polinomial_of_one_variable
+function Base. *(p1::Polynom, p2::Polynom)::Polynom
     lst = [0 for i in 1:(length(p1.coef)+length(p2.coef)-1)]
     for i in 1:length(p1.coef)
         for j in 1:length(p2.coef)
             lst[i+j-1] += p1.coef[i]*p2.coef[j]
         end
     end
-    return Polinomial_of_one_variable(lst)
+    return Polynom(lst)
 end
 
 
-
-p = Polinomial_of_one_variable([1, 3, 2])
-p1 = Polinomial_of_one_variable([4, 5, 2])
-println("------------")
-println(p.coef)
-println("------------")
-print_degree(p)
-println("------------")
-println(sum_of_pol(p, p1).coef)
-println("------------")
-println(subtraction_of_pol(p1, p).coef)
-println("------------")
-println(multiplication_of_pol(p, p1).coef)
-println("------------")
-print_poly(p)
-print_poly(subtraction_of_pol(p1, p))
-print_poly(multiplication_of_pol(p, p1))
